2fbeebf0f2af0e7a2d514223c6500855b4a1a480
VerDel Cargal <verdel@cargal.net>
2022-04-16 13:31:55 -0400

    Initial Commit

---
+++ practice 2022/practice_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  practice_2022App.swift
+//  practice 2022
+//
+//  Created by VerDel on 4/16/22.
+//
+
+import SwiftUI
+
+@main
+struct practice_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
